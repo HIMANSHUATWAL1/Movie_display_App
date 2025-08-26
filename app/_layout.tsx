c873d966018eaf3b4767d1c05503dc2dfc719dfb
Himanshu Atwal <hematwal.2003@gmail.com>
2025-08-26 17:21:43 +0530

    react-native routing and navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
 import { Stack } from "expo-router";
 import './globals.css'
 
+
+
+// it is hiding all route headers or root layout header
+
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack >
+
+       <Stack.Screen 
+       name="(tabs)"
+       options={{headerShown:false}}
+       
+       />
+       <Stack.Screen
+        name="movies/[id]"
+        options={{headerShown:false}}
+       
+       />
+
+
+
+  </Stack>;
 }
