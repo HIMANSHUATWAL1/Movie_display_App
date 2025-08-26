7700df0b327b9386a8308534aab7bd63c27daf5c
Himanshu Atwal <hematwal.2003@gmail.com>
2025-08-26 15:27:23 +0530

    tailwind setup  and configuration

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-600"
+    >
+      <Text className="text-5xl text-dark-100
+      font-bold
+      ">Welcome bro</Text>
+    </View>
+  );
+}
